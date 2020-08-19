baf2d544f324aaabecca9eb555f99c1bcfb556c9
jpbelang <jpbelang@gmail.com>
2020-08-19 10:08:45 -0400

    Ok, got some mocking.

--- src/test/js/mock_subscribed_lambda.unitspec.ts
+++ src/test/js/mock_subscribed_lambda.unitspec.ts
@@ -1,5 +1,9 @@
 import {SimpleLambdaSubscribed} from "../../main/js/subscribed_lambda";
-import {AssetCode, Function, Runtime} from "@aws-cdk/aws-lambda";
+import {Function} from "@aws-cdk/aws-lambda";
+const {Runtime, AssetCode} = jest.requireActual("@aws-cdk/aws-lambda");
+import { mocked } from 'ts-jest/utils';
+
+
 import {Queue} from "@aws-cdk/aws-sqs";
 import {Topic} from "@aws-cdk/aws-sns";
 import '@aws-cdk/assert/jest';
@@ -8,22 +12,48 @@ import {Construct, Stack} from "@aws-cdk/core";
 import {Optional} from "typescript-optional";
 import {IGrantable} from "@aws-cdk/aws-iam"
 import {haveResource} from "@aws-cdk/assert";
+import { when } from 'jest-when'
+import mock = jest.mock;
+
 
 const addEnvironmentMock = jest.fn().mockImplementation()
 jest.mock('@aws-cdk/aws-lambda', () => {
+    const functionMock = jest.fn().mockImplementation(() => {
+        return {
+            addEnvironment: addEnvironmentMock
+        };
+    });
     return {
-        Function: jest.fn().mockImplementation(() => {
-            return {
-                addEnvironment: addEnvironmentMock
-            };
-        }),
+        Function: functionMock,
         AssetCode: jest.fn().mockImplementation()
+    };
+});
+
+const grantPublishMock = jest.fn().mockImplementation()
+jest.mock('@aws-cdk/aws-sns', () => {
+    return {
+        Topic: jest.fn().mockImplementation(() => {
+            return {
+                grantPublish: grantPublishMock,
+                topicArn: "some::topic::arn"
+            };
+        })
+    };
+});
+
+const sendMessageMock = jest.fn().mockImplementation()
+jest.mock('@aws-cdk/aws-sqs', () => {
+    return {
+        Queue: jest.fn().mockImplementation(() => {
+            return {
+                grantSendMessages: sendMessageMock
+            };
+        })
     };
 });
 
 
 describe("mock subscribed lambda testing", () => {
-
 
         beforeEach(() => {
 
@@ -38,7 +68,7 @@ describe("mock subscribed lambda testing", () => {
 
             const f = new Function(new Stack(), "allo", {
                 functionName: "hello",
-                runtime: null as any,
+                runtime: Runtime.NODEJS_12_X,
                 code: AssetCode.fromInline("doodah"),
                 handler: "foo"
             })
@@ -51,23 +81,28 @@ describe("mock subscribed lambda testing", () => {
 
         it("thinkbig", () => {
 
-            AssetCode.fromInline = jest.fn()
+            const functionMock = mocked(Function, true)
 
             const lh = SimpleLambdaSubscribed.create({
                 events: ["please"], name: "my_lambda"
             })
 
+            const actualFunction = Function.prototype.constructor() as Function
             let theStack = new Stack();
             lh.handle({
                 asset: AssetCode.fromInline("doodah"),
                 deadLetterQueue: new Queue(theStack, "dead"),
                 parentConstruct: theStack,
                 parentName: "hola",
-                runtime: null as any,
+                runtime: Runtime.NODEJS_12_X,
                 topic: new Topic(theStack, "topic", {
                     topicName: "topicName"
                 })
             })
+
+            expect(addEnvironmentMock.mock.calls[0]).toEqual(["output", "some::topic::arn"])
+            expect(grantPublishMock.mock.calls[0][0]).toEqual(actualFunction)
+            expect(sendMessageMock.mock.calls[0][0]).toEqual(actualFunction)
 
         })
 
