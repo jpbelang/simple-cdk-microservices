94713062eed03c50f01703c33d832d0279476442
jpbelang <jpbelang@gmail.com>
2020-08-23 21:55:32 -0400

    Not brilliant, still need to fix compile, I think.

--- src/example/lib/example-stack.ts
+++ src/example/lib/example-stack.ts
@@ -1,11 +1,12 @@
-import {MicroserviceBuilder} from "./microservices/microservice"
-import {DynamoDBHandler} from "./microservices/dynamo_db";
+
 import {AttributeType} from "@aws-cdk/aws-dynamodb";
-import {SimpleLambdaSubscribed} from "./microservices/subscribed_lambda";
 import {AssetCode, Runtime} from "@aws-cdk/aws-lambda";
-import {simpleMethod, WebLambda} from "./microservices/web_lambda";
 import {RestApi} from "@aws-cdk/aws-apigateway";
 import {Construct, Stack, StackProps} from "@aws-cdk/core";
+import {MicroserviceBuilder} from "../../main/js/microservice";
+import {DynamoDBHandler} from "../../main/js/dynamo_db";
+import {SimpleLambdaSubscribed} from "../../main/js/subscribed_lambda";
+import {simpleMethod, WebLambda} from "../../main/js/web_lambda";
 
 export class ExampleStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
