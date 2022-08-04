198c0357f86022f9d08abf9aaacc2714348c6d4e
jpbelang <jpbelang@gmail.com>
2022-08-03 21:17:45 -0400

    exports are bad

--- src/main/js/index.ts
+++ src/main/js/index.ts
@@ -1,7 +1,22 @@
-
-export {MicroserviceBuilder, Microservice, Handler, HandlerOptions, Configurator, DefaultConfigurator, ServiceListener, NonMandatoryTaggingType, TaggingType} from './microservice'
+export {
+    MicroserviceBuilder,
+    Microservice,
+    Handler,
+    HandlerOptions,
+    Configurator,
+    DefaultConfigurator,
+    ServiceListener,
+    NonMandatoryTaggingType,
+    TaggingType,
+    snsSubscriber,
+    snsPublisher,
+    eventBridgeSubscriber,
+    eventBridgePublisher
+} from './microservice'
 export {DynamoDBHandler} from "./dynamo_db"
+export {CognitoHandler} from "./cognito_handler"
 export {SimpleLambdaSubscribed} from "./subscribed_lambda"
 export {WebLambda, simpleMethod} from "./web_lambda"
 export {DynamoStreamLambda} from "./dynamo_stream_lambda"
+export * from "./helpers/app_helpers"
 export * from "./lambda_support"
